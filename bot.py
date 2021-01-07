b1917c66086581d39757d28bd097b3a21ed85fde
chopin42 <julien.lejoly2712@gmail.com>
2021-01-07 18:07:57 +0100

    Adding debug function to it using `print("Server bumped")`

--- bot.py
+++ bot.py
@@ -5,7 +5,10 @@ class Client(discord.Client):
 	async def on_ready(self):
 		while(True):
 			time.sleep(10)
+
 			channel = self.get_channel(channel_id)
 			await channel.send("!d bump")
+
+			print("Server bumped")
 
 Client().run(user_token, bot=False)
